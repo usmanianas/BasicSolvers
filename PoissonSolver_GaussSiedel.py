ce84394b1a507a103cfc25f6cd85b85cdfcf0e07
usmanianas <anasusmani2010.com>
2021-08-22 22:06:49 +0530

    RBGS implemented

--- PoissonSolver_GaussSiedel.py
+++ PoissonSolver_GaussSiedel.py
@@ -13,7 +13,7 @@ Target = "CPU"   # CPU or GPU
 
 Lx, Ly, Lz = 1, 1, 1
 
-Nx, Ny, Nz = 32, 32, 32
+Nx, Ny, Nz = 16, 16, 16
 
 hx, hy, hz = Lx/(Nx), Ly/(Ny), Lz/(Nz)
 
@@ -26,16 +26,49 @@ idx2, idy2, idz2 = 1.0/hx2, 1.0/hy2, 1.0/hz2
 
 if Target=="CPU":
     Pp = np.zeros([Nx+2, Ny+2, Nz+2])
-    rhs = np.random.rand(Nx+2, Ny+2, Nz+2)
+    rhs = np.ones([Nx+2, Ny+2, Nz+2]) #np.random.rand(Nx+2, Ny+2, Nz+2)
     tmp = np.zeros([Nx+2, Ny+2, Nz+2])
 
     
 if Target=="GPU":
-    P = np.zeros([Nx+2, Ny+2, Nz+2])
+    Pp = np.zeros([Nx+2, Ny+2, Nz+2])
     rhs = np.random.rand(Nx+2, Ny+2, Nz+2)
     tmp = np.zeros([Nx+2, Ny+2, Nz+2])
     
 
+def Poisson_Jacobi(Pp, rho): 
+    PoissonTolerance = 1e-5  
+    jCnt = 0   
+    Pp.fill(0.0)
+    while True:
+            
+        Pp[1:-1, 1:-1, 1:-1] = (1.0/(-2.0*(idx2 + idy2 + idz2))) * (rho[1:-1, 1:-1, 1:-1] - 
+                                           idx2*(Pp[:-2, 1:-1, 1:-1] + Pp[2:, 1:-1, 1:-1]) -
+                                           idy2*(Pp[1:-1, :-2, 1:-1] + Pp[1:-1, 2:, 1:-1]) -
+                                           idz2*(Pp[1:-1, 1:-1, :-2] + Pp[1:-1, 1:-1, 2:]))   
+
+        Pp[0, :, :], Pp[-1, :, :] = -Pp[1, :, :], -Pp[-2, :, :]
+        Pp[:, 0, :], Pp[:, -1, :] = -Pp[:, 1, :], -Pp[:, -2, :]
+        Pp[:, :, 0], Pp[:, :, -1] = -Pp[:, :, 1], -Pp[:, :, -2] 
+            
+        maxErr = np.amax(np.abs(rho[1:-1, 1:-1, 1:-1] -((
+                            (Pp[:-2, 1:-1, 1:-1] - 2.0*Pp[1:-1, 1:-1, 1:-1] + Pp[2:, 1:-1, 1:-1])*idx2 +
+                            (Pp[1:-1, :-2, 1:-1] - 2.0*Pp[1:-1, 1:-1, 1:-1] + Pp[1:-1, 2:, 1:-1])*idy2 +
+                            (Pp[1:-1, 1:-1, :-2] - 2.0*Pp[1:-1, 1:-1, 1:-1] + Pp[1:-1, 1:-1, 2:])*idz2))))
+
+        if (jCnt % 100 == 0): print(jCnt, maxErr)
+
+        jCnt += 1
+        
+        if maxErr < PoissonTolerance:
+            print(jCnt)
+            break
+        
+        if jCnt > 10000:#maxCount:
+            print("ERROR: Poisson solver not converging. Aborting")
+            quit()
+        
+    return Pp  
     
 @numba.njit
 def Poisson_GS(Pp, rho, tmp, gssor):   
@@ -81,25 +114,60 @@ def Poisson_GS(Pp, rho, tmp, gssor):
     return Pp     
 
     
-
-def Poisson_Jacobi(Pp, rho, tmp, PoissonTolerance):   
+def Poisson_RBGS(Pp, rho):   
+    PoissonTolerance = 1e-5
     jCnt = 0   
     Pp.fill(0.0)
     while True:
-            
-        Pp[1:-1, 1:-1, 1:-1] = (1.0/(-2.0*(idx2 + idy2 + idz2))) * (rho[1:-1, 1:-1, 1:-1] - 
-                                           idx2*(Pp[:-2, 1:-1, 1:-1] + Pp[2:, 1:-1, 1:-1]) -
-                                           idy2*(Pp[1:-1, :-2, 1:-1] + Pp[1:-1, 2:, 1:-1]) -
-                                           idz2*(Pp[1:-1, 1:-1, :-2] + Pp[1:-1, 1:-1, 2:]))   
 
+        Pp[1:-1:2, 1:-1:2, 1:-1:2] = 1.0/(-2.0*(idx2 + idy2 + idz2))*(rho[1:-1:2, 1:-1:2, 1:-1:2] - idx2*(Pp[2::2, 1:-1:2, 1:-1:2] + Pp[:-2:2, 1:-1:2, 1:-1:2]) -
+                                                   idy2*(Pp[1:-1:2, 2::2, 1:-1:2] + Pp[1:-1:2, :-2:2, 1:-1:2]) -
+                                                   idz2*(Pp[1:-1:2, 1:-1:2, 2::2] + Pp[1:-1:2, 1:-1:2, :-2:2]))
+
+            # 1, 1, 0 configuration
+        Pp[2::2, 2::2, 1:-1:2] = 1.0/(-2.0*(idx2 + idy2 + idz2))*(rho[2::2, 2::2, 1:-1:2] - idx2*(Pp[3::2, 2::2, 1:-1:2] + Pp[1:-1:2, 2::2, 1:-1:2]) -
+                                               idy2*(Pp[2::2, 3::2, 1:-1:2] + Pp[2::2, 1:-1:2, 1:-1:2]) -
+                                               idz2*(Pp[2::2, 2::2, 2::2] + Pp[2::2, 2::2, :-2:2]))
+
+            # 1, 0, 1 configuration
+        Pp[2::2, 1:-1:2, 2::2] = 1.0/(-2.0*(idx2 + idy2 + idz2))*(rho[2::2, 1:-1:2, 2::2] - idx2*(Pp[3::2, 1:-1:2, 2::2] + Pp[1:-1:2, 1:-1:2, 2::2]) -
+                                               idy2*(Pp[2::2, 2::2, 2::2] + Pp[2::2, :-2:2, 2::2]) -
+                                               idz2*(Pp[2::2, 1:-1:2, 3::2] + Pp[2::2, 1:-1:2, 1:-1:2]))
+
+            # 0, 1, 1 configuration
+        Pp[1:-1:2, 2::2, 2::2] = 1.0/(-2.0*(idx2 + idy2 + idz2))*(rho[1:-1:2, 2::2, 2::2] - idx2*(Pp[2::2, 2::2, 2::2] + Pp[:-2:2, 2::2, 2::2]) -
+                                               idy2*(Pp[1:-1:2, 3::2, 2::2] + Pp[1:-1:2, 1:-1:2, 2::2]) -
+                                               idz2*(Pp[1:-1:2, 2::2, 3::2] + Pp[1:-1:2, 2::2, 1:-1:2]))
+
+            # Update black cells
+            # 1, 0, 0 configuration
+        Pp[2::2, 1:-1:2, 1:-1:2] = 1.0/(-2.0*(idx2 + idy2 + idz2))*(rho[2::2, 1:-1:2, 1:-1:2] - idx2*(Pp[3::2, 1:-1:2, 1:-1:2] + Pp[1:-1:2, 1:-1:2, 1:-1:2]) -
+                                                 idy2*(Pp[2::2, 2::2, 1:-1:2] + Pp[2::2, :-2:2, 1:-1:2]) -
+                                                 idz2*(Pp[2::2, 1:-1:2, 2::2] + Pp[2::2, 1:-1:2, :-2:2]))
+
+            # 0, 1, 0 configuration
+        Pp[1:-1:2, 2::2, 1:-1:2] = 1.0/(-2.0*(idx2 + idy2 + idz2))*(rho[1:-1:2, 2::2, 1:-1:2] - idx2*(Pp[2::2, 2::2, 1:-1:2] + Pp[:-2:2, 2::2, 1:-1:2]) -
+                                                 idy2*(Pp[1:-1:2, 3::2, 1:-1:2] + Pp[1:-1:2, 1:-1:2, 1:-1:2]) -
+                                                 idz2*(Pp[1:-1:2, 2::2, 2::2] + Pp[1:-1:2, 2::2, :-2:2]))
+            # 0, 0, 1 configuration
+        Pp[1:-1:2, 1:-1:2, 2::2] = 1.0/(-2.0*(idx2 + idy2 + idz2))*(rho[1:-1:2, 1:-1:2, 2::2] - idx2*(Pp[2::2, 1:-1:2, 2::2] + Pp[:-2:2, 1:-1:2, 2::2]) -
+                                                 idy2*(Pp[1:-1:2, 2::2, 2::2] + Pp[1:-1:2, :-2:2, 2::2]) -
+                                                 idz2*(Pp[1:-1:2, 1:-1:2, 3::2] + Pp[1:-1:2, 1:-1:2, 1:-1:2]))
+
+            # 1, 1, 1 configuration
+        Pp[2::2, 2::2, 2::2] = 1.0/(-2.0*(idx2 + idy2 + idz2))*(rho[2::2, 2::2, 2::2] - idx2*(Pp[3::2, 2::2, 2::2] + Pp[1:-1:2, 2::2, 2::2]) -
+                                             idy2*(Pp[2::2, 3::2, 2::2] + Pp[2::2, 1:-1:2, 2::2]) -
+                                             idz2*(Pp[2::2, 2::2, 3::2] + Pp[2::2, 2::2, 1:-1:2]))
+            
         Pp[0, :, :], Pp[-1, :, :] = -Pp[1, :, :], -Pp[-2, :, :]
         Pp[:, 0, :], Pp[:, -1, :] = -Pp[:, 1, :], -Pp[:, -2, :]
-        Pp[:, :, 0], Pp[:, :, -1] = -Pp[:, :, 1], -Pp[:, :, -2] 
+        Pp[:, :, 0], Pp[:, :, -1] = -Pp[:, :, 1], -Pp[:, :, -2]  
             
         maxErr = np.amax(np.abs(rho[1:-1, 1:-1, 1:-1] -((
                             (Pp[:-2, 1:-1, 1:-1] - 2.0*Pp[1:-1, 1:-1, 1:-1] + Pp[2:, 1:-1, 1:-1])*idx2 +
                             (Pp[1:-1, :-2, 1:-1] - 2.0*Pp[1:-1, 1:-1, 1:-1] + Pp[1:-1, 2:, 1:-1])*idy2 +
                             (Pp[1:-1, 1:-1, :-2] - 2.0*Pp[1:-1, 1:-1, 1:-1] + Pp[1:-1, 1:-1, 2:])*idz2))))
+
 
         if (jCnt % 100 == 0): print(jCnt, maxErr)
 
@@ -111,10 +179,11 @@ def Poisson_Jacobi(Pp, rho, tmp, PoissonTolerance):
         
         if jCnt > 10000:#maxCount:
             print("ERROR: Poisson solver not converging. Aborting")
-            quit()
+            break
         
-    return Pp  
-     
+    return Pp     
 
-Pp = Poisson_GS(Pp, rhs, tmp, gssor = 1.0)
-#Pp = Poisson_Jacobi(Pp, rhs, tmp, 1e-5)
+     
+#Pp = Poisson_Jacobi(Pp, rhs)
+#Pp = Poisson_GS(Pp, rhs, tmp, gssor = 1.0)
+Pp = Poisson_RBGS(Pp, rhs)
